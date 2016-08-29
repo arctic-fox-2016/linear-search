a307004d4ccffc0db7866059f11493ca668b04d4
M Haidar Hanif <git@mhaidarhanif.com>
2016-08-29 20:24:39 +0700

    Boilerplate

---
+++ linear_search.js
@@ -0,0 +1,20 @@
+// Release 0
+let linearSearch = (target, values) => {
+  //write your code here
+}
+
+let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
+
+console.log(linearSearch(18, random_numbers));
+// => 2
+console.log(linearSearch(9, random_numbers));
+
+// Release 1
+let globalLinearSearch = (target, values) => {
+  //write your code here
+}
+
+let bananas_arr = "bananas".split("");
+// => ["b", "a", "n", "a", "n", "a", "s"]
+console.log(globalLinearSearch("a", bananas_arr));
+// => [ 1, 3, 5 ]
