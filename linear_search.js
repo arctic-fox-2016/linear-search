57d5ccce18ec87f49f82776c6c6abd7d0c7bfc3e
name": null, &   "name": null <pair+elkana.sahbana@hacktiv8.com>
2016-08-30 14:57:43 +0700

    linear search

--- linear_search.js
+++ linear_search.js
@@ -1,6 +1,12 @@
 // Release 0
 let linearSearch = (target, values) => {
   //write your code here
+  for(var i=0; i<values.length; i++){
+    if(values[i]===target){
+      return values[i];
+    }
+  }
+  return -1
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -12,6 +18,18 @@ console.log(linearSearch(9, random_numbers));
 // Release 1
 let globalLinearSearch = (target, values) => {
   //write your code here
+  let result = [];
+  for(var i= 0; i<values.length; i++){
+    if(values[i]===target){
+      result.push (i)
+    }
+  }
+  if(result.length===0){
+    return -1
+  }
+  else {
+    return result
+  }
 }
 
 let bananas_arr = "bananas".split("");
